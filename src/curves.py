65cf25e5745639ae5fd35241da1c42e15f3bd883
Andreas Bock <aasbo@dtu.dk>
2022-07-03 15:39:18 +0200

    examples-shooting, curve

--- src/curves.py
+++ src/curves.py
@@ -9,11 +9,25 @@ class Curve:
     name: str
     points: np.array
 
+    def at(self, angle: float) -> List[float]:
+        pass
+
+    def __sub__(self, other) -> List[float]:
+        pass
+
 
 CURVES: List[Curve] = [
     Curve(
         name="small_triangle",
         points=[[1, 1], [3, 1], [2, 2]],
+    ),
+    Curve(
+        name="circle",
+        points=5 * np.array(
+            [
+                (np.cos(t), np.sin(t)) for t in np.linspace(0, 2*np.pi, num=25)
+            ]
+        )
     ),
     Curve(
         name="random_shape",
