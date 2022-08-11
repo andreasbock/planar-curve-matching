e75fd670b4bda4adda875b9e05e9467b9e977d53
Andreas Bock <aasbo@dtu.dk>
2022-08-11 17:45:16 +0200

    fixed momentum update

--- src/enkf.py
+++ src/enkf.py
@@ -19,7 +19,7 @@ class InverseProblemParameters:
     #   025002.
     rho: float = 0.9  # \rho \in (0, 1)
     tau: float = 1 / 0.9 + 1e-04  # \tau > 1/\rho (!)
-    gamma_scale: float = 0.1  # observation variance
+    gamma_scale: float = 1  # observation variance
     eta: float = 1e-03  # noise limit, equals ||\Lambda^{0.5}(q1-G(.))||
     relative_tolerance: float = 1e-05  # relative error to previous iteration
     sample_covariance_regularisation: float = 1  # alpha_0 regularisation parameter
@@ -75,7 +75,7 @@ class EnsembleKalmanFilter:
         while iteration < max_iterations:
             self._info(f"Iteration {iteration}: predicting...")
             shape_mean, momentum_mean, theta_mean = self.predict()
-            mismatch = target - shape_mean
+            mismatch = np.ndarray.flatten(target - shape_mean)
             new_error = self.error_norm(mismatch)
             self._info(f"Iteration {iteration}: Error norm: {new_error}")
 
@@ -91,10 +91,18 @@ class EnsembleKalmanFilter:
             if self.has_converged(new_error, previous_error):
                 break
             else:
-                self._info(f"Iteration {iteration}: correcting...")
-                shape_update, alpha = self.mismatch_covariance(mismatch)
-                self._correct_momentum(momentum_mean, mismatch, shape_update)
-                self._correct_theta(theta_mean, mismatch, shape_update)
+                centered_shape = np.ndarray.flatten(self.shape - shape_mean)
+                mismatch_local = np.ndarray.flatten(target - self.shape)
+
+                cw_alpha_gamma_inv, alpha = self.compute_cw_operator(centered_shape, mismatch, localise=False)
+
+                shape_update = np.dot(cw_alpha_gamma_inv, mismatch_local)
+                if self._inverse_problem_params.optimise_momentum:
+                    self._info(f"Iteration {iteration}: correcting momentum...")
+                    self._correct_momentum(momentum_mean, centered_shape, shape_update)
+                if self._inverse_problem_params.optimise_parameterisation:
+                    self._info(f"Iteration {iteration}: correcting parameterisation...")
+                    #self._correct_theta(theta_mean, mismatch, shape_update)
                 if self._rank == 0:
                     alphas.append(alpha)
 
@@ -108,52 +116,29 @@ class EnsembleKalmanFilter:
 
     def predict(self):
         # shoot using ensemble momenta
-        _ = self.forward_operator.shoot(self.momentum)
-        self.shape = self.forward_operator.evaluate_curve(self.parameterisation)
+        curve_result = self.forward_operator.shoot(self.momentum)
+        template_points = self.forward_operator.template.at(self.parameterisation)
+        self.shape = np.array(curve_result.diffeo.at(template_points))
 
         shape_mean = self._compute_shape_mean()
         momentum_mean = self._compute_momentum_mean()
         theta_mean = self._compute_theta_mean()
-        # TODO: think about how to dump average shape for inspection
-        #if self.verbose:
-        #    coords = Function(self.gs.VCG)
-        #    self._ensemble.allreduce(q, coords)
-        #    coords.assign(coords * self.normaliser)
-        #    old_coords = self.gs.mesh.coordinates.copy(deepcopy=True)
-        #    self.gs._update_mesh(coords)
-        #    File(self.log_dir + "w_mean_{}.pvd".format(iteration)).write(self.shape_function)
-        #    self.gs._update_mesh(old_coords)
+
         return shape_mean, momentum_mean, theta_mean
 
-    def optimise_momentum(self):
-        return self._inverse_problem_params.optimise_momentum
+    def _correct_momentum(self, momentum_mean, centered_shape, shape_update):
+        tmp = self.forward_operator.momentum_function()
+        tmp.assign(self.momentum - momentum_mean)
 
-    def optimise_parameterisation(self):
-        return self._inverse_problem_params.optimise_parameterisation
-
-    def _correct_momentum(self, momentum_mean, mismatch, shape_update):
-        self._info('Updating momentum')
-        firedrake_mismatch_no_localisation = [Constant(w) for w in np.ndarray.flatten(mismatch)]
-        #if self.localise_momentum:
-        #    for i, w in enumerate(w_flat_noloc):
-        #        self.localisers[i].interpolate(self.localiser(self.T[i//2], w))
-        #    w_flat = self.localisers
-        #else:
-        w_flat = firedrake_mismatch_no_localisation
-        m = self.forward_operator.momentum_function()
-        mr = self.forward_operator.momentum_function()
-        gain = self.forward_operator.momentum_function()
-        self.ensemble.ensemble_comm.Barrier()
-
-        for w, _s in zip(w_flat, shape_update):
-            m.assign((self.momentum - momentum_mean) * w)
-            self.ensemble.allreduce(m, mr)
-            gain.assign(gain + mr * Constant(_s))
-        self.momentum.assign(self.momentum + gain * Constant(1 / (self.ensemble_size - 1)))
+        centered_momentum = tmp.dat.data
+        c_pq = np.outer(centered_momentum, centered_shape)
+        c_pq_all = np.zeros(shape=c_pq.shape)
+        self._mpi_reduce(c_pq, c_pq_all)
+        c_pq_all /= self.ensemble_size - 1
+        gain = np.dot(c_pq_all, shape_update)
+        self.momentum = Function(self.forward_operator.DGT, val=gain)
 
     def _correct_theta(self, theta_mean, mismatch, shape_update):
-        self._info('Updating parameterisation')
-
         cov_theta = np.outer(self.parameterisation - theta_mean, mismatch)
         cov_theta_all = np.zeros(shape=cov_theta.shape)
         self._mpi_reduce(cov_theta, cov_theta_all)
@@ -162,6 +147,79 @@ class EnsembleKalmanFilter:
 
         self.parameterisation += gain
         self.parameterisation = self.parameterisation % (2 * np.pi)
+
+    def compute_cw_operator(self, centered_shape, mismatch, localise=False):
+        rhs = self._inverse_problem_params.rho * self.error_norm(mismatch)
+        self._info(f"\t rhs = {rhs}")
+
+        alpha = self._inverse_problem_params.sample_covariance_regularisation
+        cw = self.compute_cw(centered_shape, localise=localise)
+
+        iteration = 0
+        while iteration < self._inverse_problem_params.max_iter_regularisation:
+            # compute the operator of which we need the inverse
+            cw_alpha_gamma_inv = np.linalg.inv(cw + alpha * self.gamma)
+
+            # compute the error norm (lhs)
+            lhs = alpha * self.error_norm(np.dot(cw_alpha_gamma_inv, mismatch))
+            self._info(f"\t lhs = {lhs}")
+
+            if lhs >= rhs:
+                self._info(f"\t alpha = {alpha}")
+                return cw_alpha_gamma_inv, alpha
+            alpha *= 2
+            iteration += 1
+
+        error_message = f"!!! alpha failed to converge in {iteration} iterations"
+        print(error_message)
+        raise Exception(error_message)
+
+    def compute_cw(self, centered_shape, localise=False):
+        centered_shape_flat = np.ndarray.flatten(centered_shape)
+        cov_mismatch = np.outer(centered_shape_flat, centered_shape_flat)
+        cov_mismatch_all = np.zeros(shape=cov_mismatch.shape)
+        self._mpi_reduce(cov_mismatch, cov_mismatch_all)
+        if localise:
+            cov_mismatch_all = np.multiply(cov_mismatch_all, np.eye(cov_mismatch_all.shape[0]))
+        return cov_mismatch_all / (self.ensemble_size - 1)
+
+    def has_converged(self, n_err, err):
+        if n_err <= self._inverse_problem_params.tau*self._inverse_problem_params.eta:
+            self._info("Converged, error at noise level.")
+            return True
+        elif np.fabs(n_err - err) < self._inverse_problem_params.relative_tolerance:
+            self._info("No improvement in residual, terminating filter.")
+            return True
+
+    def error_norm(self, mismatch):
+        return observation_norm(self.sqrt_gamma_inv, mismatch)
+
+    def dump_parameters(self, target=None):
+        if self._rank == 0:
+
+            self._info(f"{self._inverse_problem_params}")
+            self.forward_operator.dump_parameters()
+
+            fh = open(self._logger.logger_dir / 'inverse_problem_parameters.log', 'w')
+            for key, value in asdict(self._inverse_problem_params).items():
+                fh.write(f"{key}: {value}\n")
+            fh.close()
+
+            if target is not None:
+                utils.pdump(target, self._logger.logger_dir / 'target')
+
+    def _mpi_reduce(self, f, reduced):
+        self.ensemble.ensemble_comm.Allreduce(f, reduced, op=MPI.SUM)
+
+    def _clear_cache(self):
+        self.ensemble.ensemble_comm.Barrier()
+        if self._rank == 0:
+            fd_cache = os.environ['VIRTUAL_ENV'] + '/.cache/'
+            for subdir in ['pyop2', 'tsfc']:
+                cache = fd_cache + subdir
+                if os.path.exists(cache) and os.path.isdir(cache):
+                    shutil.rmtree(cache)
+        self.ensemble.ensemble_comm.Barrier()
 
     def _compute_shape_mean(self):
         # TODO: Karcher mean?
@@ -173,7 +231,7 @@ class EnsembleKalmanFilter:
     def _compute_momentum_mean(self):
         momentum_mean = self.forward_operator.momentum_function()
         self.ensemble.allreduce(self.momentum, momentum_mean)
-        momentum_mean.assign(momentum_mean * Constant(1 / (self.ensemble_size - 1)))
+        momentum_mean.assign(momentum_mean * Constant(1 / self.ensemble_size))
         return momentum_mean
 
     def _compute_theta_mean(self):
@@ -194,86 +252,12 @@ class EnsembleKalmanFilter:
         self._mpi_reduce(_consensus_me, _consensus)
         return _consensus / self.ensemble_size
 
-    def mismatch_covariance(self, mismatch, localise=False):
-        cw_alpha_gamma_inv, alpha = self.compute_cw_operator(mismatch, localise)
-        return np.dot(cw_alpha_gamma_inv, np.ndarray.flatten(mismatch)), alpha
-
-    def compute_cw_operator(self, mismatch, localise=False):
-        _rhs = self._inverse_problem_params.rho * self.error_norm(mismatch)
-        alpha = self._inverse_problem_params.sample_covariance_regularisation
-        cw = self.compute_cw(mismatch, localise=localise)
-        iteration = 0
-        while iteration < self._inverse_problem_params.max_iter_regularisation:
-            # compute the operator of which we need the inverse
-            cw_alpha_gamma_inv = np.linalg.inv(cw + alpha * self.gamma)
-            # compute the error norm (lhs)
-            w_cw_inv = np.dot(self.sqrt_gamma, np.dot(cw_alpha_gamma_inv, np.ndarray.flatten(mismatch)))
-            _lhs = np.sqrt(np.dot(w_cw_inv, w_cw_inv))
-            if alpha * _lhs >= _rhs:
-                return cw_alpha_gamma_inv, alpha
-            alpha *= 2
-            iteration += 1
-
-        error_message = f"!!! alpha failed to converge in {iteration} iterations"
-        print(error_message)
-        raise Exception(error_message)
-
-    def compute_cw(self, mismatch, localise=False):
-        mismatch_flat = np.ndarray.flatten(mismatch)
-        cov_mismatch = np.outer(mismatch_flat, mismatch_flat)
-        cov_mismatch_all = np.zeros(shape=cov_mismatch.shape)
-        self._mpi_reduce(cov_mismatch, cov_mismatch_all)
-        if localise:
-            cov_mismatch_all = np.multiply(cov_mismatch_all, np.eye(cov_mismatch_all.shape[0]))
-        return cov_mismatch_all / (self.ensemble_size - 1)
-
-    def has_converged(self, n_err, err):
-        if n_err <= self._inverse_problem_params.tau*self._inverse_problem_params.eta:
-            self._info("Converged, error at noise level.")
-            return True
-        elif np.fabs(n_err - err) < self._inverse_problem_params.relative_tolerance:
-            self._info("No improvement in residual, terminating filter.")
-            return True
-
-    def error_norm(self, mismatch):
-        return observation_norm(self.sqrt_gamma_inv, mismatch)
-
-    def gamma_norm(self, mismatch):
-        return observation_norm(self.sqrt_gamma, mismatch)
-
-    def _mpi_reduce(self, f, reduced):
-        self.ensemble.ensemble_comm.Allreduce(f, reduced, op=MPI.SUM)
-
-    def _clear_cache(self):
-        self.ensemble.ensemble_comm.Barrier()
-        if self._rank == 0:
-            fd_cache = os.environ['VIRTUAL_ENV'] + '/.cache/'
-            for subdir in ['pyop2', 'tsfc']:
-                cache = fd_cache + subdir
-                if os.path.exists(cache) and os.path.isdir(cache):
-                    shutil.rmtree(cache)
-        self.ensemble.ensemble_comm.Barrier()
-
-    def dump_parameters(self, target=None):
-        if self._rank == 0:
-
-            self._info(f"{self._inverse_problem_params}")
-            self.forward_operator.dump_parameters()
-
-            fh = open(self._logger.logger_dir / 'inverse_problem_parameters.log', 'w')
-            for key, value in asdict(self._inverse_problem_params).items():
-                fh.write(f"{key}: {value}\n")
-            fh.close()
-
-            if target is not None:
-                utils.pdump(target, self._logger.logger_dir / 'target')
-
     def _info(self, msg):
         if self._rank == 0:
             self._logger.info(msg)
 
 
 def observation_norm(sqrt_matrix, x):  # TODO: check order of this!
-    inner_norm = np.dot(sqrt_matrix, np.ndarray.flatten(x))
+    inner_norm = np.dot(sqrt_matrix, x)
     return np.sqrt(np.dot(inner_norm, inner_norm))
 
