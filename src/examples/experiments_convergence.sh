cd1ffdb63bcc8834da3a5464df85a2373cadda5b
Andreas Bock <aasbo@dtu.dk>
2022-09-20 17:02:28 +0200

    default 8 esize for convergence

--- src/examples/experiments_convergence.sh
+++ src/examples/experiments_convergence.sh
@@ -3,7 +3,7 @@
 export OMP_NUM_THREADS=1
 export PYTHONPATH=../..
 
-ENSEMBLE_SIZE=4
+ENSEMBLE_SIZE=8
 mpiexec -n $ENSEMBLE_SIZE python experiments_convergence.py
 
 python ../plot_pickles.py ../../RESULTS_CONVERGENCE
