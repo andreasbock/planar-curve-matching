3683602d2b34bef5340736a04f1d34ce4db86135
Andreas Bock <aasbo@dtu.dk>
2022-10-04 12:22:37 +0200

    ensemble size script

--- src/examples/experiments_ensemble_size.sh
+++ src/examples/experiments_ensemble_size.sh
@@ -5,12 +5,12 @@ export PYTHONPATH=../..
 
 ENSEMBLE_SIZE=8
 mpiexec -n $ENSEMBLE_SIZE python example_enkf.py
-python ../plot_pickles.py ../../RESULTS_EXPERIMENTS
+python ../plot_pickles.py ../../RESULTS_EXAMPLES_ENKF_ESIZE=$ENSEMBLE_SIZE
 
 ENSEMBLE_SIZE=16
 mpiexec -n $ENSEMBLE_SIZE python example_enkf.py
-python ../plot_pickles.py ../../RESULTS_EXPERIMENTS
+python ../plot_pickles.py ../../RESULTS_EXAMPLES_ENKF_ESIZE=$ENSEMBLE_SIZE
 
 ENSEMBLE_SIZE=32
 mpiexec -n $ENSEMBLE_SIZE python example_enkf.py
-python ../plot_pickles.py ../../RESULTS_EXPERIMENTS
+python ../plot_pickles.py ../../RESULTS_EXAMPLES_ENKF_ESIZE=$ENSEMBLE_SIZE
