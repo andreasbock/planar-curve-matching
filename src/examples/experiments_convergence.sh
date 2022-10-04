eb2691125219d1eab1d821c479b140393f161794
Andreas Bock <aasbo@dtu.dk>
2022-10-04 12:14:14 +0200

    removed duplicate

--- src/examples/experiments_convergence.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-export OMP_NUM_THREADS=1
-export PYTHONPATH=../..
-
-ENSEMBLE_SIZE=8
-mpiexec -n $ENSEMBLE_SIZE python experiments_convergence.py
-
-python ../plot_pickles.py ../../RESULTS_CONVERGENCE
