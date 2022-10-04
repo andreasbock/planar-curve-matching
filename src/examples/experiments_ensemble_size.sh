965b2129fb6a8cf52ae0542629f83258bf842585
Andreas Bock <aasbo@dtu.dk>
2022-10-04 12:13:36 +0200

    ensemble convergence

--- src/examples/experiments_ensemble_size.sh
+++ src/examples/experiments_ensemble_size.sh
@@ -4,13 +4,13 @@ export OMP_NUM_THREADS=1
 export PYTHONPATH=../..
 
 ENSEMBLE_SIZE=8
-mpiexec -n $ENSEMBLE_SIZE python experiments_ensemble_size.py
+mpiexec -n $ENSEMBLE_SIZE python example_enkf.py
 python ../plot_pickles.py ../../RESULTS_EXPERIMENTS
 
 ENSEMBLE_SIZE=16
-mpiexec -n $ENSEMBLE_SIZE python experiments_ensemble_size.py
+mpiexec -n $ENSEMBLE_SIZE python example_enkf.py
 python ../plot_pickles.py ../../RESULTS_EXPERIMENTS
 
 ENSEMBLE_SIZE=32
-mpiexec -n $ENSEMBLE_SIZE python experiments_ensemble_size.py
+mpiexec -n $ENSEMBLE_SIZE python example_enkf.py
 python ../plot_pickles.py ../../RESULTS_EXPERIMENTS
