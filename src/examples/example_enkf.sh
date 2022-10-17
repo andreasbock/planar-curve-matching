65526287383f6781aa2fe2868f14bf72b7086210
Andreas Bock <aasbo@dtu.dk>
2022-10-17 11:54:32 +0200

    changelist oopsie

--- src/examples/example_enkf.sh
+++ src/examples/example_enkf.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 
-ENSEMBLE_SIZE=16
 export OMP_NUM_THREADS=1
 export PYTHONPATH=../..
 
+ENSEMBLE_SIZE=5
 mpiexec -n $ENSEMBLE_SIZE python example_enkf.py
 python ../plot_pickles.py ../../RESULTS_EXAMPLES_ENKF_ESIZE=$ENSEMBLE_SIZE
 
