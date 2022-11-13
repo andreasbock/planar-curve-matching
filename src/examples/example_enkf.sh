a772fb86b483e30a9fd15434b1b1d2f91b2b9f18
Andreas Bock <aasbo@dtu.dk>
2022-11-13 10:21:12 +0100

    ensemble size=10

--- src/examples/example_enkf.sh
+++ src/examples/example_enkf.sh
@@ -3,7 +3,7 @@
 export OMP_NUM_THREADS=1
 export PYTHONPATH=../..
 
-ENSEMBLE_SIZE=5
+ENSEMBLE_SIZE=10
 mpiexec -n $ENSEMBLE_SIZE python example_enkf.py
 python ../plot_pickles.py ../../RESULTS_EXAMPLES_ENKF_ESIZE=$ENSEMBLE_SIZE
 
