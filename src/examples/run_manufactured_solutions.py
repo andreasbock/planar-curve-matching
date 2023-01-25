ea660f963e8c1af51d5069720fd316d2090122f9
Andreas Bock <aasbo@dtu.dk>
2023-01-25 16:35:54 +0100

    nan=1.0 reformat

--- src/examples/run_manufactured_solutions.py
+++ src/examples/run_manufactured_solutions.py
@@ -51,7 +51,10 @@ if __name__ == "__main__":
                         dont_raise=True,
                     )
                 )
-                indicator_moved_original_mesh.dat.data[:] = np.nan_to_num(indicator_moved_original_mesh.dat.data[:], nan=1.)
+                indicator_moved_original_mesh.dat.data[:] = np.nan_to_num(
+                    indicator_moved_original_mesh.dat.data[:],
+                    nan=1.0,
+                )
                 utils.my_heaviside(indicator_moved_original_mesh)
                 utils.plot_curves(indicator_moved_original_mesh, path / f"{mesh_path.stem}_{momentum.name}.pdf")
 
